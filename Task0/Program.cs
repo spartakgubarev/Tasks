77a27f3054c36900fd8c9c4b4cf52c16d67880ec
spartakgubarev <spartakgubarev@rambler.ru>
2022-07-28 15:51:30 +0300

    add Tasks02

---
+++ Task0/Program.cs
@@ -0,0 +1,29 @@
+﻿// Почувствуй себя интерном.
+
+//  0. Вывести квадрат числа
+// Console.Clear();
+// Console.Write("Введите число, возведу в квадрат: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{number} в квадрате = {number*number}");
+
+//  1. По двум заданным числам проверять является ли первое квадратом второго
+// Console.Clear();
+// Console.WriteLine("Введите два числа, проверю, является ли первое число квадратом второго");
+// Console.Write("Ввдите первое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Ввдите второе число: ");
+// int numberPow = Convert.ToInt32(Console.ReadLine());
+// if (number*number==numberPow) Console.WriteLine ($"{numberPow} является квадратом {number}");
+// else  Console.WriteLine ($"{numberPow} не является квадратом {number}");
+
+//  2. Даны два числа. Показать большее и меньшее число
+// Console.Clear();
+// Console.WriteLine("Введите два числа, проверю, какое больше, а какое меньше");
+// Console.Write("Ввдите первое число: ");
+// int numberFirst = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Ввдите второе число: ");
+// int numberSecond = Convert.ToInt32(Console.ReadLine());
+// if (numberFirst>numberSecond) Console.WriteLine($"Число {numberSecond} меньше {numberFirst}");
+// else Console.WriteLine($"Число {numberSecond} больше {numberFirst}");
+
+//  3. По заданному номеру дня недели вывести его название
