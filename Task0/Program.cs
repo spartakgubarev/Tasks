6de46879322c46306dc3c10db987bb711e68dfb7
spartakgubarev <spartakgubarev@rambler.ru>
2022-07-29 17:51:24 +0300

    add Task0.2

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -27,3 +27,76 @@
 // else Console.WriteLine($"Число {numberSecond} больше {numberFirst}");
 
 //  3. По заданному номеру дня недели вывести его название
+Console.Clear();
+
+//  4. Найти максимальное из трех чисел
+//  5. Написать программу вычисления значения функции y = f(a)
+//  6. Выяснить является ли число чётным
+//  7. Показать числа от -N до N
+//  8. Показать четные числа от 1 до N
+//  9. Показать последнюю цифру трёхзначного числа
+// 10. Показать вторую цифру трёхзначного числа
+// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+// 12. Удалить вторую цифру трёхзначного числа
+// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+// 14. Найти третью цифру числа или сообщить, что её нет
+
+
+// Треугольник Паскаля *** от Сергея Каменского
+// Console.Clear();
+// int row = 10;
+// int[,] triagle = new int[row, row];
+// const int cellWidth = 5;
+
+// void FillTriagle()
+// {
+//     for (int i = 0; i < row; i++)
+//     {
+//         triagle[i, 0] = 1;
+//         triagle[i, i] = 1;
+//     }
+
+//     for (int i = 2; i < row; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             triagle[i, j] =
+//                 triagle[i - 1, j - 1] + triagle[i - 1, j];
+//         }
+//     }
+// }
+
+// void PrintTriagle()
+// {
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < row; j++)
+//         {
+//             if (triagle[i, j] != 0)
+//                 Console.Write($"{triagle[i, j],cellWidth}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// void Magic()
+// {
+//     int col = cellWidth * row;
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j <= i; j++)
+//         {
+//             Console.SetCursorPosition(col, i + 1);
+//             // if (triagle[i,j]!=0) Console.Write($"{triagle[i,j],cellWidth}");
+//             if (triagle[i, j] != 0) Console.WriteLine("*");
+//             col += cellWidth * 2;
+//         }
+//         col = cellWidth * row - cellWidth * (i + 1);
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.ReadLine();
+// FillTriagle();
+// // PrintTriagle();
+// Console.ReadLine();
+// Magic();
