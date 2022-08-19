b7454aa95d4bacf4b0c3d47e45f3c4849b4f6603
spartakgubarev <spartakgubarev@rambler.ru>
2022-08-19 19:26:43 +0300

    Добавлен быстрая сортировка

--- Tasks01/Program.cs
+++ Tasks01/Program.cs
@@ -305,3 +305,43 @@ MIN = 2
 //     Console.Write("\nКонечный массив:\t[" + string.Join(", ", array) + "]");
 // }
 // Console.ReadKey();
+
+// Быстрая сортировка
+/*
+1. arr = {1, 0, -6, 2, 5, 3, 2} 
+2. pivot = arr[6]  (опорный элемент)
+3. Вызвать шаги 1-2 для подмассива слева от pivot и справа от pivot
+*/
+int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
+Console.Write($"Массив {string.Join(", ", arr)}\n");
+QuickSort(arr, 0, arr.Length - 1);
+Console.Write($"Отсортированный массив {string.Join(", ", arr)}");
+void QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if (minIndex >= maxIndex) return;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return;
+}
+int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for (int i = minIndex; i <= maxIndex; i++)
+    {
+        if (inputArray[i] < inputArray[maxIndex])
+        {
+            pivotIndex++;
+            Swap(inputArray, i, pivotIndex);
+        }
+    }
+    pivotIndex++;
+    Swap(inputArray, pivotIndex, maxIndex);
+    return pivotIndex;
+}
+void Swap(int[] inputArray, int leftValue, int rightValue)
+{
+    int temp = inputArray[leftValue];
+    inputArray[leftValue] = inputArray[rightValue];
+    inputArray[rightValue] = temp;
+}
